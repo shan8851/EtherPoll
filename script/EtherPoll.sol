f2dd6ffaa5445b4690026b1379763f284d64229b
shan8851 <asamshan456@gmail.com>
2025-04-29 09:30:33 +0100

    chore: format

--- script/EtherPoll.sol
+++ script/EtherPoll.sol
@@ -5,15 +5,15 @@ import {Script, console} from "forge-std/Script.sol";
 import {EtherPoll} from "../src/EtherPoll.sol";
 
 contract DeployEtherPoll is Script {
-  EtherPoll public etherPoll;
+    EtherPoll public etherPoll;
 
-  function run() external {
-    uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+    function run() external {
+        uint256 deployerKey = vm.envUint("PRIVATE_KEY");
 
-    vm.startBroadcast(deployerKey);
+        vm.startBroadcast(deployerKey);
 
-    etherPoll = new EtherPoll();
+        etherPoll = new EtherPoll();
 
-    vm.stopBroadcast();
-  }
+        vm.stopBroadcast();
+    }
 }
